845aad1b6ef615417803222bcc8d1c77cc84c630
watersmark <DuldinEV@mail.ru>
2020-10-07 13:39:02 +0300

    max elem product

--- Numeral System Converter/Problems (1)/The maximum product of adjacent elements/src/Main.java
+++ Numeral System Converter/Problems (1)/The maximum product of adjacent elements/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+        int sum = 0;
+        int elem_last = 0;
+
+        for (int i = 0; i < count; i++) {
+
+            if (i == 0) {
+                elem_last = scanner.nextInt();
+                continue;
+            }
+
+            int elem_temp = scanner.nextInt();
+
+            if (sum < elem_last * elem_temp) {
+                sum = elem_last * elem_temp;
+            }
+
+            elem_last = elem_temp;
+
+        }
+
+        System.out.println(sum);
+
     }
 }
