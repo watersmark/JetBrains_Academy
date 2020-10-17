74b96f080af38afe6cb1ba74378b6619c8870b57
EVGENII <watersmark@mail.ru>
2020-10-17 15:56:33 +0300

    Init project

---
+++ Numeral System Converter1/Problems/Celsius to Fahrenheit/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        double C = scanner.nextDouble();
+
+        System.out.println(C * 1.8 + 32);
+
+    }
+}
